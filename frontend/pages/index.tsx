719c96845db4b88979608123c276bd92cf0b5b3f
teoyuqi <teo.yuqi@gmail.com>
2023-09-17 21:41:41 +0800

    Align font with Figma

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,17 @@
 import Navbar from "@/components/PeerPrepNavbar";
+import cx from "classnames";
 import { useSession } from "next-auth/react";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: "400", subsets: ["latin-ext"] });
 
 export default function Home() {
   const userInfo = useSession().data?.user;
   return (
     <main
-      className={`flex min-h-screen flex-col items-center ${inter.className}`}
-    >
+      className={cx(
+        "flex min-h-screen flex-col items-center",
+        poppins.className)}>
       <Navbar />
       <div>
         <h1>Main Page</h1>
