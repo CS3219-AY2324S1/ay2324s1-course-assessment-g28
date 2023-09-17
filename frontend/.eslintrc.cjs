9016655b9bed42b9d3ae269f7e60e7e5b0b23889
teoyuqi <teo.yuqi@gmail.com>
2023-09-17 20:05:03 +0800

    Add additional eslint rules for FE

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,7 +4,45 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
   ],
+  env: {
+    "browser": true,
+    "es2021": true,
+    "node": true,
+  },
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    "ecmaVersion": "latest",
+    "sourceType": "module",
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react",
+  ],
   root: true,
+  rules: {
+    "max-len": 1,
+    "indent": [ 1, 2 ],
+    "no-tabs": 0,
+    "quotes": [ 1, "double" ],
+    "semi": [ 1, "always" ],
+    "@typescript-eslint/semi": [1],
+    "no-trailing-spaces": 1,
+    "comma-dangle": [ 1, "always-multiline" ],
+    "object-curly-spacing": [ 1, "always" ],
+    "array-bracket-spacing": [ 1, "always", { "singleValue": false } ],
+    "@typescript-eslint/no-unused-vars": 1,
+    "@typescript-eslint/member-delimiter-style": [
+      "warn",
+      {
+        "multiline": {
+          "delimiter": "semi",
+          "requireLast": true,
+        },
+        "singleline": {
+          "delimiter": "semi",
+          "requireLast": false,
+        },
+      },
+    ],
+  },
 };
