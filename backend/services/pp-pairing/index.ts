6c7917591d0ed737bba7ed6e2cf5c89968871113
zaiahkoh <zaiah0505@hotmail.com>
2023-09-18 00:02:23 +0800

    initial commit for pp-pairing

--- backend/services/pp-pairing/index.ts
+++ backend/services/pp-pairing/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import userRouter from './src/routes/user'
+import pairingRouter from './src/routes/pairing'
 
 dotenv.config();
 
@@ -8,9 +8,9 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Express + TypeScript Server');
 });
-app.use('/user', userRouter)
+app.use('/pairing', pairingRouter)
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
