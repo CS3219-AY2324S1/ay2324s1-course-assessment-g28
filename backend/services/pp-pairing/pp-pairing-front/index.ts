87570ddc340157fa27f5bc95b75fae1b870ff2eb
zaiahkoh <zaiah0505@hotmail.com>
2023-09-18 17:17:12 +0800

    integrating rmq and ws

--- backend/services/pp-pairing/pp-pairing-front/index.ts
+++ backend/services/pp-pairing/pp-pairing-front/index.ts
@@ -1,17 +1,49 @@
 import { IncomingMessage } from "http";
 import url from "url";
 import { WebSocketServer, WebSocket } from "ws";
+import amqp from "amqplib/callback_api";
+import dotenv from "dotenv";
 
-const wss = new WebSocketServer({ port: 8080, path: "/path" });
+dotenv.config();
+const RABBITMQ_URL = process.env.RABBITMQ_URL!;
 
-wss.on("connection", function connection(ws: WebSocket, req: IncomingMessage) {
-  console.log(url.parse(req.url!, true).query);
+console.log(RABBITMQ_URL)
 
-  ws.on("error", console.error);
+amqp.connect(RABBITMQ_URL, function (error0, rmq_connection) {
+  if (error0) {
+    throw error0;
+  }
 
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
-  });
+  const wss = new WebSocketServer({ port: 8080, path: "/pairing" });
 
-  ws.send("something");
+  wss.on(
+    "connection",
+    function connection(ws: WebSocket, req: IncomingMessage) {
+      console.log(url.parse(req.url!, true).query);
+
+      rmq_connection.createChannel(function (error1, channel) {
+        if (error1) {
+          throw error1;
+        }
+
+        var queue = "hello";
+        var msg = "Hello world";
+
+        channel.assertQueue(queue, {
+          durable: false,
+        });
+
+        channel.sendToQueue(queue, Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
+
+        ws.on("message", function message(data) {
+          channel.sendToQueue(queue, Buffer.from(data.toString()));
+        });
+      });
+
+      ws.on("error", console.error);
+
+      ws.send("something");
+    }
+  );
 });
--- backend/services/pp-pairing/pp-pairing-front/test-client.js
+++ backend/services/pp-pairing/pp-pairing-front/test-client.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket('ws://localhost:8080/path?foo=bar&swag=true');
+const ws = new WebSocket('ws://localhost:8080/pairing?foo=bar&swag=true');
 
 ws.on('error', console.error);
 
