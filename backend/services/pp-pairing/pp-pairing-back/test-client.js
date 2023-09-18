c3638b1985bfd42cd23d163fa3f7616f1964b6ab
zaiahkoh <zaiah0505@hotmail.com>
2023-09-18 16:59:39 +0800

    updated packages

---
+++ backend/services/pp-pairing/pp-pairing-back/test-client.js
@@ -0,0 +1,25 @@
+const amqp = require("amqplib/callback_api");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const RABBITMQ_URL = process.env.RABBITMQ_URL;
+
+amqp.connect(RABBITMQ_URL, function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var queue = "hello";
+    var msg = "Hello world";
+
+    channel.assertQueue(queue, {
+      durable: false,
+    });
+
+    channel.sendToQueue(queue, Buffer.from(msg));
+    console.log(" [x] Sent %s", msg);
+  });
+});
