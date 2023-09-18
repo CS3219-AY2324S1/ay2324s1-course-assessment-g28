9c118401762ae54bc4027b2c6c7e8557e7b82d75
zaiahkoh <zaiah0505@hotmail.com>
2023-09-18 23:28:49 +0800

    connected logic from client to backend

--- backend/services/pp-pairing/pp-pairing-back/index.ts
+++ backend/services/pp-pairing/pp-pairing-back/index.ts
@@ -1,21 +1,41 @@
-import amqp from 'amqplib/callback_api'
+import amqp from "amqplib/callback_api";
+import dotenv from "dotenv";
 
-amqp.connect('amqp://localhost', function(error0, connection) {
+dotenv.config();
+const RABBITMQ_URL = process.env.RABBITMQ_URL!;
+
+console.log(`Connected to ${RABBITMQ_URL}`);
+
+amqp.connect(RABBITMQ_URL, function (error0, connection) {
   if (error0) {
     throw error0;
   }
-  connection.createChannel(function(error1, channel) {
+  console.log(`Connected to ${RABBITMQ_URL}`);
+
+  connection.createChannel(function (error1, channel) {
     if (error1) {
       throw error1;
     }
-    var queue = 'hello';
-    var msg = 'Hello world';
 
+    var queue = "pairing_requests";
     channel.assertQueue(queue, {
-      durable: false
+      durable: false,
     });
+    console.log(`Awaiting pairing requests on queue: {${queue}}`);
 
-    channel.sendToQueue(queue, Buffer.from(msg));
-    console.log(" [x] Sent %s", msg);
+    channel.consume(queue, function (msg) {
+      var reply = {
+        data: "Bonjour",
+      };
+      channel.sendToQueue(
+        msg!.properties.replyTo,
+        Buffer.from(JSON.stringify(reply)),
+        {
+          correlationId: msg!.properties.correlationId,
+        }
+      );
+
+      channel.ack(msg!);
+    });
   });
 });
---
+++ backend/services/pp-pairing/pp-pairing-back/src/controllers/user-pairing.ts
@@ -0,0 +1,24 @@
+import { List } from "linked-list";
+import { User } from "../models/user-list";
+
+function isMatch(user1: User, user2: User): boolean {
+  // not implemented yet
+  return true;
+}
+
+function matchUser(userList: List<User>, user: User): null | [User, User] {
+  let curr = userList.head;
+
+  while (curr) {
+    if (isMatch(curr, user)) {
+      curr.detach();
+      return [curr, user];
+    }
+  }
+
+  userList.append(user);
+
+  return null;
+}
+
+export default matchUser;
