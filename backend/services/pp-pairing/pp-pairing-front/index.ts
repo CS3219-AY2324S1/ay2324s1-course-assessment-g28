50d997fe0b0c9bd87ace706209abdf78ee47d2d5
zaiahkoh <zaiah0505@hotmail.com>
2023-09-18 16:59:39 +0800

    updated packages

--- backend/services/pp-pairing/pp-pairing-front/index.ts
+++ backend/services/pp-pairing/pp-pairing-front/index.ts
@@ -1,19 +1,17 @@
-import { IncomingMessage } from 'http';
-import url from 'url';
-import { URLSearchParams } from 'url';
-import { WebSocketServer, WebSocket } from 'ws';
+import { IncomingMessage } from "http";
+import url from "url";
+import { WebSocketServer, WebSocket } from "ws";
 
-const wss = new WebSocketServer({ port: 8080, path: '/path' });
+const wss = new WebSocketServer({ port: 8080, path: "/path" });
 
-wss.on('connection', function connection(ws: WebSocket, req: IncomingMessage) {
-
+wss.on("connection", function connection(ws: WebSocket, req: IncomingMessage) {
   console.log(url.parse(req.url!, true).query);
 
-  ws.on('error', console.error);
+  ws.on("error", console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
   });
 
-  ws.send('something');
+  ws.send("something");
 });
