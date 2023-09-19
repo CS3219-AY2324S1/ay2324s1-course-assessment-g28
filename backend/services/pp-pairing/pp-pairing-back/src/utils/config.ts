7ed35f6732face6d67836d453b2224b73704bb80
zaiahkoh <zaiah0505@hotmail.com>
2023-09-19 12:48:47 +0800

    added eviction logic for old user matches

--- backend/services/pp-pairing/pp-pairing-back/src/utils/config.ts
+++ backend/services/pp-pairing/pp-pairing-back/src/utils/config.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export default {
   RABBITMQ_URL: process.env.RABBITMQ_URL!,
   LOGGING_LEVEL: process.env.LOGGING_LEVEL || "info",
+  MATCHMAKING_MAX_WAIT_SECONDS: Number(process.env.MATCHMAKING_MAX_WAIT) || 100,
 };
