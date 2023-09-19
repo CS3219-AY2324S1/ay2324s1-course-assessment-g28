5efdae7a5cee9463c09d552ca2d1e75656237d43
zaiahkoh <zaiah0505@hotmail.com>
2023-09-19 16:31:16 +0800

    changed pp-pairing-back to async await pattern

--- backend/services/pp-pairing/pp-pairing-back/index.ts
+++ backend/services/pp-pairing/pp-pairing-back/index.ts
@@ -1,4 +1,4 @@
-import amqp from "amqplib/callback_api";
+import amqp from "amqplib";
 import { User } from "./src/models/user";
 import { List } from "./src/models/linked-list";
 import matchUser from "./src/controllers/user-pairing";
@@ -6,69 +6,62 @@ import MockEditor from "./src/services/editor/mock-editor";
 import logger from "./src/utils/logger";
 import config from "./src/utils/config";
 
-amqp.connect(config.RABBITMQ_URL, function (error0, connection) {
-  if (error0) {
-    throw error0;
-  }
+amqp.connect(config.RABBITMQ_URL).then(async (rmq_conn) => {
   logger.info(`Connected to ${config.RABBITMQ_URL}`);
 
-  connection.createChannel(function (error1, channel) {
-    if (error1) {
-      throw error1;
-    }
+  let editorService = new MockEditor();
+  let userList = new List<User>();
 
-    let editorService = new MockEditor();
-    let userList = new List<User>();
-
-    var queue = "pairing_requests";
-    channel.assertQueue(queue, {
+  let channel = await rmq_conn.createChannel();
+  let [request_queue, cancel_queue] = await Promise.all([
+    channel.assertQueue("pairing_requests", {
       durable: false,
-    });
-    logger.info(`Awaiting pairing requests on queue: {${queue}}`);
+    }),
+    channel.assertQueue("pairing_cancels", { durable: true }),
+  ]);
 
-    channel.consume(queue, async function (msg) {
-      logger.debug(
-        `Received pairing request on queue: {${queue}}, with content {${msg?.content.toString()}, with correlationId {${
-          msg?.properties.correlationId
-        }}}`
+  channel.consume(request_queue.queue, async (msg) => {
+    logger.debug(
+      `Received pairing request on queue: {${
+        request_queue.queue
+      }}, with content {${msg?.content.toString()}, with correlationId {${
+        msg?.properties.correlationId
+      }}}`
+    );
+
+    let content = JSON.parse(msg!.content.toString());
+    let user = new User(
+      {
+        replyTo: msg!.properties.replyTo,
+        correlationId: msg!.properties.correlationId,
+      },
+      content.match_options
+    );
+    let match = matchUser(userList, user);
+    channel.ack(msg!);
+
+    if (match) {
+      let url = await editorService.postPair(
+        match[0].match_options.user,
+        match[1].match_options.user
       );
 
-      let user = new User(
-        {
-          replyTo: msg!.properties.replyTo,
-          correlationId: msg!.properties.correlationId,
-        },
-        JSON.parse(msg!.content.toString())
-      );
-
-      let match = matchUser(userList, user);
-
-      channel.ack(msg!);
-
-      if (match) {
-        let url = await editorService.postPair(
-          match[0].match_options.user,
-          match[1].match_options.user
+      let reply = {
+        url,
+      };
+      match.forEach((m) => {
+        channel.sendToQueue(
+          m.reply_params.replyTo,
+          Buffer.from(JSON.stringify(reply)),
+          {
+            correlationId: m.reply_params.correlationId,
+          }
         );
 
-        let reply = {
-          url,
-        };
-
-        match.forEach((m) => {
-          channel.sendToQueue(
-            m.reply_params.replyTo,
-            Buffer.from(JSON.stringify(reply)),
-            {
-              correlationId: m.reply_params.correlationId,
-            }
-          );
-
-          logger.debug(
-            `Sending to correlationId: {${m.reply_params.correlationId}}, with message {${reply}}`
-          );
-        });
-      }
-    });
+        logger.debug(
+          `Sending to correlationId: {${m.reply_params.correlationId}}, with message {${reply}}`
+        );
+      });
+    }
   });
 });
