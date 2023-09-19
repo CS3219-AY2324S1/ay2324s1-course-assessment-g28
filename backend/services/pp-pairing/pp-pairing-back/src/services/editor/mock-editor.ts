7b82d95aa7b0c8fe872a6336cab67dfa69f04c84
zaiahkoh <zaiah0505@hotmail.com>
2023-09-19 11:48:12 +0800

    settled linked-list

--- backend/services/pp-pairing/pp-pairing-back/src/services/editor/mock-editor.ts
+++ backend/services/pp-pairing/pp-pairing-back/src/services/editor/mock-editor.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
+import EditorService from "./editor-service";
 
 class MockEditor implements EditorService {
   async postPair(_user1: string, _user2: string): Promise<string> {
