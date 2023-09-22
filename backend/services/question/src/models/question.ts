bf1153438f9d7d3aee2c21a269d3ab4970bffe54
Gerald Neo <gerald.ner56@gmail.com>
2023-09-22 17:46:47 +0800

    make title unique for question

--- backend/services/question/src/models/question.ts
+++ backend/services/question/src/models/question.ts
@@ -11,7 +11,7 @@ interface IQuestion extends Document {
 
 const questionSchema = new mongoose.Schema<IQuestion>({
   id: { type: Number, unique: true, index: true },
-  title: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   category: { type: [String], required: true },
   complexity: { type: Number, enum: [0, 1, 2], required: true },
