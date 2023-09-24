62b515296a09103129bb54cdcbf85ac92171178e
zaiahkoh <zaiah0505@hotmail.com>
2023-09-24 20:23:11 +0800

    added env for pairing-back

--- backend/services/pairing/pairing-back/src/utils/config.ts
+++ backend/services/pairing/pairing-back/src/utils/config.ts
@@ -4,7 +4,6 @@ dotenv.config();
 
 export default {
   RABBITMQ_URL: process.env.RABBITMQ_URL!,
-  PORT: process.env.PORT || 3000,
   LOGGING_LEVEL: process.env.LOGGING_LEVEL || "info",
   MATCHMAKING_MAX_WAIT_SECONDS: Number(process.env.MATCHMAKING_MAX_WAIT) || 100,
 };
