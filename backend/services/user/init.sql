61ce7ce89455ac7b83f6582c9dd5d5ac20258d57
Gerald Neo <gerald.ner56@gmail.com>
2023-09-27 20:42:17 +0800

    basic user api endpoint creation

--- backend/services/user/init.sql
+++ backend/services/user/init.sql
@@ -2,5 +2,15 @@
 
 CREATE TABLE IF NOT EXISTS Users (
     email TEXT PRIMARY KEY,
-    name TEXT
+    username TEXT UNIQUE NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS Attempts (
+    id serial PRIMARY KEY,
+    email TEXT REFERENCES Users(email),
+    question_id INT NOT NULL,
+    question_title TEXT NOT NULL,
+    attempt_date TIMESTAMP NOT NULL DEFAULT NOW(),
+    attempt_details TEXT NOT NULL
 );
--- backend/services/user/src/controller.ts
+++ backend/services/user/src/controller.ts
@@ -14,17 +14,48 @@ const pool = new Pool({
   port: Number(PG_PORT) || 5432,
 });
 
+//POST handlers
 export const createUser = async (req: Request, res: Response) => {
   try {
-    const { email, name } = req.body;
-    const query = "INSERT INTO Users (email, name) VALUES ($1, $2) RETURNING *";
-    const result = await pool.query(query, [email, name]);
-    res.status(200).json(result.rows[0]);
+    const { email, username } = req.body;
+    const query =
+      "INSERT INTO Users (email, username) VALUES ($1, $2) RETURNING *";
+    const result = await pool.query(query, [email, username]);
+    res.status(204).json(result.rows[0]);
   } catch (error) {
     res.status(500).json({ error: `createUser failed ${error}` });
   }
 };
 
+export const createAttempt = async (req: Request, res: Response) => {
+  try {
+    const { email } = req.params;
+    const { questionId, questionTitle, attemptDetails, attemptDate } = req.body;
+
+    let query, queryArgs;
+
+    if (attemptDate) {
+      query = `INSERT INTO Attempts (email, question_id, question_title, attempt_date, attempt_details) VALUES ($1, $2, $3, $4, $5) RETURNING * `;
+      queryArgs = [
+        email,
+        questionId,
+        questionTitle,
+        attemptDate,
+        attemptDetails,
+      ];
+    } else {
+      query = `INSERT INTO Attempts (email, question_id, question_title, attempt_details) VALUES ($1, $2, $3, $4) RETURNING * `;
+      queryArgs = [email, questionId, questionTitle, attemptDetails];
+    }
+
+    const result = await pool.query(query, queryArgs);
+    res.status(204).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: `createAttempt failed ${error}` });
+  }
+};
+
+//GET handlers
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
     const query = "SELECT * FROM Users";
@@ -35,11 +66,42 @@ export const getAllUsers = async (req: Request, res: Response) => {
   }
 };
 
+export const getUserByEmail = async (req: Request, res: Response) => {
+  try {
+    const { email } = req.params;
+    const userQuery = "SELECT username, is_admin FROM Users WHERE email = $1";
+    const userResult = await pool.query(userQuery, [email]);
+
+    const attemptQuery =
+      "SELECT id AS attempt_id, question_id, question_title, attempt_date FROM Attempts WHERE email = $1";
+    const attemptResult = await pool.query(attemptQuery, [email]);
+    res.status(200).json({
+      ...userResult.rows[0],
+      attemptedQuestions: attemptResult.rows,
+    });
+  } catch (error) {
+    res.status(500).json({ error: `getAllUsers failed ${error}` });
+  }
+};
+
+export const getAttemptById = async (req: Request, res: Response) => {
+  try {
+    const { email, attemptId } = req.params;
+    const query =
+      "SELECT id AS attempt_id, question_id, question_title, attempt_date, attempt_details FROM Attempts WHERE email = $1 and id = $2";
+    const result = await pool.query(query, [email, attemptId]);
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: `getAllUsers failed ${error}` });
+  }
+};
+
+//PUT/PATCH handlers
 export const updateUserByEmail = async (req: Request, res: Response) => {
   try {
-    const { email, name } = req.body;
-    const query = "UPDATE Users SET name = $1 WHERE email = $2 RETURNING *";
-    const result = await pool.query(query, [name, email]);
+    const { email, username } = req.body;
+    const query = "UPDATE Users SET username = $1 WHERE email = $2 RETURNING *";
+    const result = await pool.query(query, [username, email]);
     if (result.rows.length === 0) {
       res.status(404).json({ message: `${email} does not exist.` });
     } else {
@@ -50,9 +112,10 @@ export const updateUserByEmail = async (req: Request, res: Response) => {
   }
 };
 
+// DELETE handlers
 export const deleteUserByEmail = async (req: Request, res: Response) => {
   try {
-    const { email } = req.body;
+    const { email } = req.params;
     const query = "DELETE FROM Users WHERE email = $1 RETURNING *";
     const result = await pool.query(query, [email]);
     if (result.rows.length === 0) {
@@ -61,6 +124,6 @@ export const deleteUserByEmail = async (req: Request, res: Response) => {
       res.status(200).json({ message: `${email} deleted successfully!` });
     }
   } catch (error) {
-    res.status(500).json({ error: `createUser failed ${error}` });
+    res.status(500).json({ error: `deleteUsers failed ${error}` });
   }
 };
