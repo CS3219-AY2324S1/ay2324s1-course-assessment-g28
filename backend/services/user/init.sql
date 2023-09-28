88682e501807eeaa5150e08d5a203709d2a5623d
Gerald Neo <gerald.ner56@gmail.com>
2023-09-28 12:27:03 +0800

    add favourite_programming_language field and question attempt counts by difficulty

--- backend/services/user/init.sql
+++ backend/services/user/init.sql
@@ -3,6 +3,7 @@
 CREATE TABLE IF NOT EXISTS Users (
     email TEXT PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
+    favourite_programming_language TEXT,
     is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
@@ -11,6 +12,7 @@ CREATE TABLE IF NOT EXISTS Attempts (
     email TEXT REFERENCES Users(email),
     question_id INT NOT NULL,
     question_title TEXT NOT NULL,
+    question_difficulty INT NOT NULL,
     attempt_date TIMESTAMP NOT NULL DEFAULT NOW(),
     attempt_details TEXT NOT NULL
 );
