9879099c4ec7c0abf6468dd8079d051edef90c07
teoyuqi <teo.yuqi@gmail.com>
2023-10-02 02:49:18 +0800

    Fix ts errors

--- frontend/api/server/user.ts
+++ frontend/api/server/user.ts
@@ -6,12 +6,12 @@ import { HttpStatus } from "@/api/constants";
 import { USER_API } from "@/api/routes";
 import { User, UserZod } from "@/api/user/types";
 
-class UserDoesNotExist extends Error {}
+class UserDoesNotExist extends Error { }
 
 export async function getUserInfoServerSide(userEmail: string) {
   //TODO: Remove when finished dev
   if (process.env.USER_BACKEND_MODE === "LOCAL") {
-    return { isAdmin: true };
+    return { isAdmin: true } as User;
   }
   try {
     const res = await fetch(
