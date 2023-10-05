74d8b08e87541c75ce4602556d1f803e4d79b4c5
benmurphyy <47269122+benmurphyy@users.noreply.github.com>
2023-10-05 18:48:10 +0800

    Remove hardcoding of pairing service

--- frontend/api/pairing/index.ts
+++ frontend/api/pairing/index.ts
@@ -1,12 +1,6 @@
 import { PairingRequest } from "@/api/pairing/types";
 
 export function getPairingServiceUri(pairingRequest: PairingRequest) {
-  return `ws://localhost:4000/pairing?user=${pairingRequest.userId}`;
-}
-
-// TODO: remove harcoded values
-const PAIRING_SERVICE = "ws://localhost:4000/pairing";
-
-export function getRequestEndpoint(pairingRequest: PairingRequest): string {
-  return `${PAIRING_SERVICE}?user=${pairingRequest}`;
+  //TODO: check that we can get NEXT_PUBLIC_PAIRING_API correctly for non-local setups, as client needs direct access to pairing front
+  return `${process.env.NEXT_PUBLIC_PAIRING_API}/pairing?user=${pairingRequest.userId}`;
 }
