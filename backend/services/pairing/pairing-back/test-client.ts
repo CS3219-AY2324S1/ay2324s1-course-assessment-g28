5ba1e70de8de662726b3328cb4f31e631a61203f
zaiahkoh <zaiah0505@hotmail.com>
2023-10-06 16:35:16 +0800

    Removed unused test-clients

--- backend/services/pairing/pairing-back/test-client.ts
+++
@@ -1,9 +0,0 @@
-import { Item, List } from "./src/models/linked-list";
-
-class Something extends Item {}
-
-let test: Item = new Something();
-
-let things = new List<Something>();
-
-console.log(Number("asdf") || 100)
