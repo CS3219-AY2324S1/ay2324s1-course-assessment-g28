2d686511e0ca870d8e46bed9901651c458115558
Gerald Neo <gerald.ner56@gmail.com>
2023-10-12 19:57:32 +0800

    add pg port to admin script

--- backend/services/user/scripts/makeUserAdmin.js
+++ backend/services/user/scripts/makeUserAdmin.js
@@ -3,10 +3,10 @@ const pgp = require("pg-promise")();
 require("dotenv").config({ path: __dirname + "/../.env" });
 
 const db = pgp(
-  `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:5432/user`
+  `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:${process.env.PG_PORT}/user`
 );
 
-emailsToMakeAdmin = ["abc@gmail.com"];
+emailsToMakeAdmin = [];
 
 for (let email of emailsToMakeAdmin) {
   db.none(`UPDATE Users SET is_admin = true WHERE email = '${email}'`)
