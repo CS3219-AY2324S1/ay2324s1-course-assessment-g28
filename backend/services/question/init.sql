1daaf00395e1c64e8a3b6031ff97379623d88589
Gerald Neo <gerald.ner56@gmail.com>
2023-10-17 17:52:47 +0800

    remove sql from local backend docker file

--- backend/services/question/init.sql
+++
@@ -1,7 +0,0 @@
--- this script will be run during initialisation
-
-CREATE TABLE IF NOT EXISTS Images (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    question_id INTEGER,
-    image_data BYTEA
-);
