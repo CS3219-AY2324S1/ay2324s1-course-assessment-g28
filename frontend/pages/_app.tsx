7fad8e4b75c68af3deb5f8143a80fc2917236ecd
teoyuqi <teo.yuqi@gmail.com>
2023-10-22 14:38:28 +0800

    fix toast

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -12,8 +12,8 @@ export default function App({
   return (
     <NextUIProvider>
       <SessionProvider session={session}>
+        <Toaster />
         <Layout>
-          <Toaster />
           <Component {...pageProps} />
         </Layout>
       </SessionProvider>
