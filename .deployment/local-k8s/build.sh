40b437542526ddf9e3495c43d35dc7aa3d105e76
zaiahkoh <zaiah0505@hotmail.com>
2023-10-28 12:01:59 +0800

    completed local-k8s

--- .deployment/local-k8s/build.sh
+++ .deployment/local-k8s/build.sh
@@ -2,8 +2,9 @@
 cd "$(dirname "$0")"
 cd ../..
 
-docker build -t peerprep/frontend ./frontend/
+docker build -t peerprep/frontend ./frontend/ --build-arg="NEXT_PUBLIC_PAIRING_API=ws://localhost:4000"
 docker build -t peerprep/user ./backend/services/user/
 docker build -t peerprep/question ./backend/services/question/
 docker build -t peerprep/pairing-front ./backend/services/pairing/pairing-front/
 docker build -t peerprep/pairing-back ./backend/services/pairing/pairing-back/
+docker build -t peerprep/collab ./backend/services/collab/
