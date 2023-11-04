57f78b583eed1ee4aa0ae16f3a7867f9e1a1731a
benmurphyy <47269122+benmurphyy@users.noreply.github.com>
2023-11-05 01:17:52 +0800

    Setup up single editor toolbar

--- frontend/components/SingleEditor/index.tsx
+++ frontend/components/SingleEditor/index.tsx
@@ -2,16 +2,22 @@ import { useRef, useState } from "react";
 import Editor, { Monaco } from "@monaco-editor/react";
 import { PanelGroup, Panel } from "react-resizable-panels";
 import HorizontalResizeHandle from "@/components/PanelResizeHandles/HorizontalResizeHandle";
-import { ProgrammingLanguage } from "@/components/SingleEditor/constants";
+import {
+  CodeRunState,
+  ProgrammingLanguage,
+  programmingLanguageMonacoIdentifiers,
+} from "@/components/SingleEditor/constants";
 import { useTheme } from "next-themes";
+import { Button, Select, SelectItem, Spinner } from "@nextui-org/react";
 
 export default function SingleEditor() {
   const editorRef = useRef(null);
   const { theme } = useTheme();
-  const [codeRunResult, setCodeRunResult] = useState<string>(
-    'Click "Run" to execute your code!',
-  );
-  const [language, setLanguage] = useState<ProgrammingLanguage>("java");
+  const [codeRunState, setCodeRunState] = useState<CodeRunState>({
+    result: 'Click "Run" to execute your code!',
+    inProgress: false,
+  });
+  const [language, setLanguage] = useState<ProgrammingLanguage>("Java");
 
   function handleEditorDidMount(editor: any, monaco: Monaco) {
     // here is the editor instance
@@ -22,19 +28,53 @@ export default function SingleEditor() {
     <PanelGroup direction="vertical" className="flex flex-grow">
       <Panel className="flex flex-col" defaultSize={80}>
         <div className="flex flex-col flex-grow">
-          <div></div>
+          <div className="flex flex-row gap-2 items-center w-full pb-2">
+            <Select
+              size="sm"
+              placeholder="Select Programming Langauge"
+              selectedKeys={[language]}
+              onChange={(e) => {
+                if (!e.target.value) {
+                  return;
+                }
+                setLanguage(e.target.value as ProgrammingLanguage);
+              }}
+            >
+              {Object.keys(programmingLanguageMonacoIdentifiers).map((lang) => (
+                <SelectItem key={lang} value={lang}>
+                  {lang}
+                </SelectItem>
+              ))}
+            </Select>
+            <Button disabled={codeRunState.inProgress} color="primary">
+              Run Code
+            </Button>
+            <Button disabled={codeRunState.inProgress} color="secondary">
+              Submit
+            </Button>
+          </div>
           <Editor
             theme={theme === "light" ? "light" : "vs-dark"}
             className="flex-grow"
-            language={language}
+            language={programmingLanguageMonacoIdentifiers[language]}
             defaultValue="// some comment"
-            options={{ minimap: { enabled: false }, fontSize: 18 }}
+            options={{ minimap: { enabled: false }, fontSize: 16 }}
             onMount={handleEditorDidMount}
           />
         </div>
       </Panel>
       <HorizontalResizeHandle />
-      <Panel className="p-2">{codeRunResult}</Panel>
+      <Panel>
+        {codeRunState.inProgress ? (
+          <div className="w-full h-full grid content-center bg-content1 rounded-md">
+            <Spinner label="Executing code..." color="secondary"></Spinner>
+          </div>
+        ) : (
+          <div className="h-full w-full bg-content1 rounded-md p-4">
+            {codeRunState.result}
+          </div>
+        )}
+      </Panel>
     </PanelGroup>
   );
 }
