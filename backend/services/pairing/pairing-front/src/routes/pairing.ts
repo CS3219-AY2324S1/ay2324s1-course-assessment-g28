f9f53c5c2730b45d3e78d2c7ea2ba58a48c4052f
Gerald Neo <gerald.ner56@gmail.com>
2023-11-13 12:03:58 +0800

    remove extra services

--- backend/services/pairing/pairing-front/src/routes/pairing.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import { get } from "../controllers/pairing";
-
-const express = require("express");
-const router = express.Router();
-
-router.get("/", get);
-
-export default router;
