cafd24a865e10a09f25bb9ea442ff20aaaf7f8a2
teoyuqi <teo.yuqi@gmail.com>
2023-11-15 15:52:36 +0800

    fix day/month ordering

--- frontend/components/UserProfileCard/config.ts
+++ frontend/components/UserProfileCard/config.ts
@@ -1,6 +1,5 @@
 import { User } from "@/api/user/types";
 
-export const BE_DATE_FORMAT = "YYYY-DD-MM";
 export const CHART_DATE_FORMAT = "YYYY-MM-DD";
 export const DATETIME_FORMAT = "YYYY-MM-DD LTS";
 export const UNDEFINED_VALUE = "--";
