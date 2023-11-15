28db786a47b450cbad37aeedc804192dcfdbeece
Gerald Neo <gerald.ner56@gmail.com>
2023-11-15 13:30:44 +0800

    change script mongo uri

--- backend/services/question/scripts/populate_sample_data.js
+++ backend/services/question/scripts/populate_sample_data.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const fs = require("fs");
 
-const MONGO_URI = "mongodb://localhost:27017";
+const MONGO_URI = "mongodb://localhost:27017/questions";
 
 const questionSchema = new mongoose.Schema({
   id: { type: Number, unique: true, index: true },
