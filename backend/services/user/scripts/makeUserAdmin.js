c480379f165510f298eec4447e60a6c7b133db05
benmurphyy <47269122+benmurphyy@users.noreply.github.com>
2023-11-15 13:02:59 +0800

    Fix admin script to use env

--- backend/services/user/scripts/makeUserAdmin.js
+++ backend/services/user/scripts/makeUserAdmin.js
@@ -1,11 +1,13 @@
 const pgp = require("pg-promise")();
+require("dotenv").config({ path: __dirname + "/../.env" });
 
-const POSTGRES_USER = "dev";
-const POSTGRES_PASSWORD = "dev";
-const PG_PORT = "5432";
+const POSTGRES_USER = process.env.POSTGRES_USER;
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+const PG_PORT = process.env.PG_PORT;
+const POSTGRES_URI = process.env.POSTGRES_URI;
 
 const db = pgp(
-  `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:${PG_PORT}/user`
+  `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_URI}:${PG_PORT}/user`
 );
 
 if (process.argv.length < 3) {
